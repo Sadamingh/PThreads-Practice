a3e086adbb2baf45e0ebaed45710b800644040be
Sadamingh <yxing11@usfca.edu>
2021-02-03 17:23:54 +0800

    add creation and terminate

---
+++ 01-Create-And-Termination/CAT.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <unistd.h>
+#include <pthread.h>
+#include <errno.h>
+using namespace std;
+
+void *thread(void *ptr)
+{   
+    cout << "PThead: This is a pthread." << endl;
+    sleep(1);
+    return 0;
+}
+
+int main() {
+    // Create a thread.
+    cout << "Creating a PThread..." << endl;
+    pthread_t thread1;
+    if (pthread_create(&thread1, NULL, thread, NULL)) {    // eqvivalent to thread1 = fork(proc, args)
+        perror("ERROR");
+        exit(0);
+    } else {
+        cout << "Successfully created!" << endl;
+    }
+
+    // Terminate the thread.
+    cout << "Block the current thread..." << endl;
+    pthread_join(thread1, NULL);       // eqvivalent to join()
+    cout << "End of execution." << endl;
+
+    return 0;
+}
